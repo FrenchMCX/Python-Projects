ee5d0fd23724c80c826ef275bd9f27e305d7ceb8
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-06-03 16:09:20 -0400

    .

---
+++ ScriptAssignment.py
@@ -0,0 +1,43 @@
+import os
+currentDirectory = os.getcwd()
+path = "C:\Users\tbone\OneDrive\Desktop\Python Directory"
+list_of_filenames_in_path_directory = os.listdir(path)
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt1.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt1.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt1.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt2.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt2.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt2.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt3.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt3.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt3.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt4.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt4.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt4.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt5.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt5.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt5.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt6.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt6.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt6.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt7.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt7.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt7.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt8.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt8.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt8.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt9.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt9.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt9.txt)
+
+join_example = os.path.join("C:\Users\tbone\OneDrive\Desktop\Python Directory", "C:\Users\tbone\OneDrive\Desktop\Python Directory\txt10.txt")
+join == "C:\Users\tbone\OneDrive\Desktop\Python Directory\\C:\Users\tbone\OneDrive\Desktop\Python-Projects\\C:\Users\tbone\OneDrive\Desktop\Python Directory\txt10.txt"  # is true
+date_file_was_last_modified = os.path.getmtime(txt10.txt)
