629685e62550962d132e1827bd293f7172605909
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-07-05 13:52:31 -0400

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,12 @@
+class Zoo:
+    name = 'No Name Provided'
+    location = ' '
+
+class Animal(Zoo):
+    habitat = ' '
+    diet = ' '
+
+class Attractions(Zoo):
+    cost = '$ '
+    attractionType = ' '
+    attractionName = ' '
